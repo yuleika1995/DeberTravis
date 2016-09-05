d80bcbd1eec986350f025d0961e53de80daceabd
yuleika1995 <y.ulei15@hotmail.com>
2016-09-05 11:15:34 -0500

    Iniciando variables

---
+++ DeberTravis/src/debertravis/DeberTravis.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package debertravis;
+
+/**
+ *
+ * @author Andrés
+ */
+public class DeberTravis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+          int numero1=10; //variables enteras
+        int numero2=5;  //variables enteras
+
+    }
+    
+}
