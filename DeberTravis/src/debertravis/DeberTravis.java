b3da3b11ac969544fab4dd00c04d3e1fb1d846e7
yuleika1995 <y.ulei15@hotmail.com>
2016-09-05 11:20:41 -0500

    Agregado operacion multiplicacion y format

--- DeberTravis/src/debertravis/DeberTravis.java
+++ DeberTravis/src/debertravis/DeberTravis.java
@@ -16,16 +16,19 @@ public class DeberTravis {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-          int numero1=10; //variables enteras
-        int numero2=5;  //variables enteras
- int suma=numero1+numero2; 
-       System.out.println("la suma es"+suma);
-       
-        int resta=numero1-numero2;
-      System.out.println("la suma es"+resta);
-      
-       int divicion=numero1/numero2;
-     System.out.println("la suma es"+divicion) ;
+        int numero1 = 10; //variables enteras
+        int numero2 = 5;  //variables enteras
+        int suma = numero1 + numero2;
+        System.out.println("la suma es" + suma);
+
+        int resta = numero1 - numero2;
+        System.out.println("la suma es" + resta);
+
+        int divicion = numero1 / numero2;
+        System.out.println("la suma es" + divicion);
+
+        int multiplicacion = numero1 * numero2;
+        System.out.println("la suma es" + suma);
     }
     
 }
